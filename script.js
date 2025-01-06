c8e2393847dcf21a04e9457e0522d65a3110a62f
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-06 16:54:05 +0100

    de
    fr

---
+++ script.js
@@ -0,0 +1,24 @@
+// let nbre1 = 12;
+//  let nbre4 = 5;
+//  console.log(typeof (nbre1));
+ 
+
+//  let lets = "Eva"
+//  console.log(typeof(lets));
+
+// console.log(lets + ' a ' + nbre1 + ' ans');
+
+ console.log("Somme de deux nombre");
+ 
+  let nbre1 = 12;
+  let nbre2 = 5;
+  let somme = nbre1 + nbre2;
+  console.log('La somme est de ' + somme);
+  console.log('La somme e de ' + nbre1 +  ' et ' +nbre2 + ' est de ' + somme);
+  
+  console.log("Soustraction de deux nombre");
+  let soustraction = nbre1 - nbre2;
+  console.log('La soustraction est de ' + soustraction);
+
+
+
