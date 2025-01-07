97a84d890c25f7fa0955734eafb487928eb8495e
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-07 15:06:45 +0100

    base
    base

--- script.js
+++ script.js
@@ -8,17 +8,51 @@
 
 // console.log(lets + ' a ' + nbre1 + ' ans');
 
- console.log("Somme de deux nombre");
+//  console.log("Somme de deux nombre");
  
-  let nbre1 = 12;
-  let nbre2 = 5;
-  let somme = nbre1 + nbre2;
-  console.log('La somme est de ' + somme);
-  console.log('La somme e de ' + nbre1 +  ' et ' +nbre2 + ' est de ' + somme);
+//   let nbre1 = 12;
+//   let nbre2 = 5;
+//   let somme = nbre1 + nbre2;
+//   console.log('La somme est de ' + somme);
+//   console.log('La somme e de ' + nbre1 +  ' et ' +nbre2 + ' est de ' + somme);
   
-  console.log("Soustraction de deux nombre");
-  let soustraction = nbre1 - nbre2;
-  console.log('La soustraction est de ' + soustraction);
+//   console.log("Soustraction de deux nombre");
+//   let soustraction = nbre1 - nbre2;
+//   console.log('La soustraction est de ' + soustraction);
+
+let nbre1 = 12;
+
+let nbre2 = 5;
+let string = "Eva";
+let nbre3 = "12";
+let som = nbre1 - nbre2;
+console.log(nbre1 !== nbre3);
+console.log(`La soustraction rrrrrrrrrrrrrrrrrrrrrrrrrr
+ jjjjjjjjjjjjjjjj
+ llllllllllllll
+  rrrrrrrrrrrrrrrrrest de ` + som);
+console.log(som);
+
+
+
+
+
+let nbre02 =parseInt( prompt("Entrez le premier nombre"));
+let nbre03 = parseInt(prompt("Entrez le deuxième nombre"));
+let som1 =nbre02 / nbre03;
+
+// alert("La somme est de " + som1);
+alert(`La somme  ${nbre02} / ${nbre03} = ${som1}`);
+
+
+console.log(som1);
+
+ alert("Bonjour alert");
+prompt("Bonjour prompt");
+document.write("Bonjour document");
+
+
+
 
 
 
